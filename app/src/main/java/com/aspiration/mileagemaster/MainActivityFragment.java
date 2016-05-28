eaf4a564cd579d267bc515bbc040e336af1ce4d0
jonathan.cook <nyorksjc@hotmail.com>
2016-05-28 08:20:16 -0400

    AdMob complete

--- app/src/main/java/com/aspiration/mileagemaster/MainActivityFragment.java
+++ app/src/main/java/com/aspiration/mileagemaster/MainActivityFragment.java
@@ -1,16 +1,20 @@
 package com.aspiration.mileagemaster;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private AdView mAdView;
 
     public MainActivityFragment() {
     }
@@ -18,7 +22,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View viewroot = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mAdView = (AdView) viewroot.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        return viewroot;
     }
 
 
